3203fbe8d2b4bd88250640ed6b47591928180769
Marcelo Santos <rocha.mer21@gmail.com>
2021-07-11 15:03:34 -0300

    Interface Funcional

--- src/introducao/application/MyComparator.java
+++ src/introducao/application/MyComparator.java
@@ -1,4 +1,18 @@
 package introducao.application;
 
-public class MyComparator {
+import introducao.entities.Product;
+
+import java.util.Comparator;
+
+/**
+ * Interface Funcional
+ * É uma interface que possui um único método abstrato. Suas
+ * implementações serão tratadas como expressões lambda.
+ * **/
+
+public class MyComparator implements Comparator<Product> {
+    @Override
+    public int compare(Product p1, Product p2) {
+        return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+    }
 }
